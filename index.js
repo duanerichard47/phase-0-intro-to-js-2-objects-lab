541bc5f522124d8a713dfe25c4cab6af3fbaaa11
Duane <duanerichard47@hotmail.com>
2022-12-23 15:07:53 -0500

    commit tree 087010ea42d927fc3c26803e7def2394b6fa38d4

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+
+
+
+const employee = {
+    name: 'employee',
+    streetAddress: '11 Broadway'
+    };
+//First task
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+    }
+    
+const new_employee1 = updateEmployeeWithKeyAndValue(employee, "name", "Sam")
+console.log(new_employee1.name)
+
+
+//Second Task
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+const new_employee2 = updateEmployeeWithKeyAndValue(employee, "name", "Sam")
+console.log(new_employee2.name)
+
+
+// Third Task
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee};
+    delete newObj.name;
+    return newObj;
+    }
+    
+let newEmployee = deleteFromEmployeeByKey(employee, "name")
+
+console.log(newEmployee)
+
+//Fourth Task
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
+let newEmployee2 = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+console.log(newEmployee2)
